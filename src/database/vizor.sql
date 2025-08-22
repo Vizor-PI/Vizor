1e1a91c94d237d327bb5b850dc10ded2da88cdef
AdamanLino <adaman.santos@sptech.school>
2025-08-22 20:04:55 -0300

    feat: login funcional

--- src/database/vizor.sql
+++ src/database/vizor.sql
@@ -27,6 +27,7 @@ create table miniPc (
 create table endereco (
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	rua varchar(300) NOT NULL,
+    numero INT NOT NULL,
     cep CHAR(8) NOT NULL,
     fkMinipc INT,
     foreign key (fkMinipc) references miniPc(id)
@@ -37,3 +38,10 @@ create table alertas (
     fkMinipc INT,
     foreign key (fkMinipc) references miniPc(id)
 );
+
+-- inserts para teste de login
+insert into empresa (nome, cnpj) 
+	values ('Empresa A', '12345678000190');
+
+insert into usuario (nome, email, senha, fkEmpresa) 
+	values ('asd', 'teste@teste.com', '123', 1);
