ab999c3bbd8fcb3c967261d047f1056cecb7108c
guilhermef-roveri <guilherme.roveri@sptech.school>
2025-09-01 14:02:16 -0300

    Fix: arquivo banco e fethc json

--- src/models/dadosUsuarioModel.js
+++ src/models/dadosUsuarioModel.js
@@ -1,23 +1,47 @@
 var database = require("../database/config");
 
 function listarDados(id) {
-  console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarDados()");
+  console.log("ACESSEI O usuario MODEL \n\n >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n >> verifique suas credenciais de acesso ao banco\n >> e se o servidor de seu BD está rodando corretamente.\n\n function listarDados()");
+
   var instrucaoSql = `
-        SELECT us.nome as NomeUsuario, us.email as EmailUsuario, us.telefone as Telefone, em.nome as NomeEmpresa,us.cpf as CPF, us.senha as SenhaUsuario
+        SELECT 
+            us.nome       AS NomeUsuario, 
+            us.email      AS EmailUsuario, 
+            us.telefone   AS Telefone, 
+            us.cpf        AS CPF, 
+            us.senha      AS SenhaUsuario,
+            em.nome       AS NomeEmpresa,
+            cg.titulo     AS Cargo
         FROM usuario AS us
-        INNER JOIN empresa AS em on us.fkEmpresa = em.id WHERE us.id = '${id}';
+        INNER JOIN empresa AS em 
+            ON us.fkEmpresa = em.id
+        INNER JOIN cargo AS cg
+            ON us.fkCargo = cg.id
+        WHERE us.id = ${id};
     `;
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
 }
 
-function listarUsuarios(){
-  console.log("Acessei o model listarUsuarios")
-  var instrucaoSql = 
-  `
-  SELECT us.nome as NomeUsuario, us.email as EmailUsuario, us.telefone as Telefone, us.cpf as CPF, us.senha as SenhaUsuario , us.cargo FROM usuario as us;
-  `
-  console.log("Executando a instrução SQL")
+function listarUsuarios() {
+  console.log("Acessei o model listarUsuarios");
+
+  var instrucaoSql = `
+        SELECT 
+            us.nome       AS NomeUsuario, 
+            us.email      AS EmailUsuario, 
+            us.telefone   AS Telefone, 
+            us.cpf        AS CPF, 
+            us.senha      AS SenhaUsuario,
+            em.nome       AS NomeEmpresa,
+            cg.titulo     AS Cargo
+        FROM usuario AS us
+        INNER JOIN empresa AS em 
+            ON us.fkEmpresa = em.id
+        INNER JOIN cargo AS cg
+            ON us.fkCargo = cg.id;
+    `;
+  console.log("Executando a instrução SQL");
   return database.executar(instrucaoSql);
 }
 
@@ -25,4 +49,3 @@ module.exports = {
   listarDados,
   listarUsuarios
 }
-
