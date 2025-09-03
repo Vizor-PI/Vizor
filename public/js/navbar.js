dce76785dfc3fa9a1bae4d7ca1b6eda45c8d5345
guilhermef-roveri <guilherme.roveri@sptech.school>
2025-09-03 20:52:06 -0300

    fix: Separação de arquivos da dash e siteinstitucional

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -3,7 +3,7 @@
  <!-- Logo -->
       <a href="#">
         <img
-          src="assets/imgs/logosDash/logoDashboard.png"
+          src="../assets/imgs/logosDash/logoDashboard.png"
           alt=""
           class="imagemLogoDashboard"
         />
@@ -12,35 +12,35 @@
       <!-- Menu -->
       <ul id="textosNavbar">
       <li class="nav-item">
-        <img src="assets/imgs/logosDash/home.png" alt="" id="logoNavbarDash" />
+        <img src="../assets/imgs/logosDash/home.png" alt="" id="logoNavbarDash" />
         <a href="inicioDash.html" style="color: aqua"> Início </a>
       </li>
       <li class="nav-item">
-        <img src="assets/imgs/logosDash/logoGestao.png" alt="" id="logoNavbarDash" />
+        <img src="../assets/imgs/logosDash/logoGestao.png" alt="" id="logoNavbarDash" />
         <a href="gestaoUsuarios.html"> Painel </a>
       </li>
       <li class="nav-item">
-        <img src="assets/imgs/logosDash/dashboardIcon.png" alt="" id="logoNavbarDash" />
+        <img src="../assets/imgs/logosDash/dashboardIcon.png" alt="" id="logoNavbarDash" />
         <a href="dashboard.html"> Dashboard </a>
       </li>
       <li class="nav-item">
-        <img src="assets/imgs/logosDash/avisos.png" alt="" id="logoNavbarDash" />
+        <img src="../assets/imgs/logosDash/avisos.png" alt="" id="logoNavbarDash" />
         <a href="avisoDash.html"> Avisos </a>
       </li>
       <li class="nav-item">
-        <img src="assets/imgs/logosDash/mudarContaIcon.png" alt="" id="logoNavbarDash" />
+        <img src="../assets/imgs/logosDash/mudarContaIcon.png" alt="" id="logoNavbarDash" />
         <a href="gestaoUsuarios.html"> Alterar dados </a>
       </li>
       <li class="nav-item">
-        <img src="assets/imgs/logosDash/configIcon.png" alt="" id="logoNavbarDash" />
+        <img src="../assets/imgs/logosDash/configIcon.png" alt="" id="logoNavbarDash" />
         <a href="infoConta.html"> Info. da conta </a>
       </li>
     </ul>
 
       <!-- Botão Sair -->
       <div id="divSair">
-        <img src="assets/imgs/logosDash/exitIcon.png" id="logoNavbarDash" />
-        <a href="./index.html" onclick="limparSessao()"> Sair </a>
+        <img src="../assets/imgs/logosDash/exitIcon.png" id="logoNavbarDash" />
+        <a href="../index.html" onclick="limparSessao()"> Sair </a>
       </div>
  `
  
