7daea47783c50b4b134ebebba1759b8024b18414
Joao-do-Carmo <joao.carmo@sptech.school>
2025-09-03 20:49:27 -0300

    Feat: tela de cadastro de empresa (html, css e rotas)

--- src/controllers/empresaController.js
+++ src/controllers/empresaController.js
@@ -1,12 +1,40 @@
 var empresaModel = require("../models/empresaModel");
 
 function buscarDadosEmpresa(req, res) {
-
   empresaModel.buscarDadosEmpresa().then((resultado) => {
     res.status(200).json(resultado);
   });
 }
 
+function cadastrar(req, res) {
+  var nome = req.body.nomeServer;
+  var cnpj = req.body.cnpjServer;
+  var codigo = req.body.codigoServer;
+
+  if (nome == undefined) {
+    res.status(400).send("Seu nome está undefined!");
+  } else if (cnpj == undefined) {
+    res.status(400).send("Seu email está undefined!");
+  } else if (codigo == undefined) {
+    res.status(400).send("Sua senha está undefined!");
+  } else {
+    empresaModel
+      .cadastrar(nome, cnpj, codigo)
+      .then(function (resultado) {
+        res.json(resultado);
+      })
+      .catch(function (erro) {
+        console.log(erro);
+        console.log(
+          "\nHouve um erro ao realizar o cadastro! Erro: ",
+          erro.sqlMessage
+        );
+        res.status(500).json(erro.sqlMessage);
+      });
+  }
+}
+
 module.exports = {
-  buscarDadosEmpresa
+  buscarDadosEmpresa,
+  cadastrar,
 };
