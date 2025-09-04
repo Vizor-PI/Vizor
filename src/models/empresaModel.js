f92ac866794834a933a0514c35b950bf81796ab4
Joao-do-Carmo <joao.carmo@sptech.school>
2025-09-03 21:03:46 -0300

    Revert "Feat: tela de cadastro de empresa (html, css e rotas)"

--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -6,21 +6,4 @@ function buscarDadosEmpresa() {
   return database.executar(instrucaoSql);
 }
 
-function cadastrar(nome, cnpj, codigo) {
-  console.log(
-    "ACESSEI O EMPRESA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",
-    nome,
-    cnpj,
-    codigo
-  );
-  var instrucaoSql = `
-        INSERT INTO empresa (nome, cnpj, codigoAtivacao) VALUES ('${nome}', '${cnpj}', '${codigo}');
-    `;
-  console.log("Executando a instrução SQL: \n" + instrucaoSql);
-  return database.executar(instrucaoSql);
-}
-
-module.exports = {
-  buscarDadosEmpresa,
-  cadastrar,
-};
+module.exports = {buscarDadosEmpresa};
