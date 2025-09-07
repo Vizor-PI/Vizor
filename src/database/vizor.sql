8e75001cababb8d1d8f4affd4e4590b539cc9411
Joao-do-Carmo <joao.carmo@sptech.school>
2025-09-07 11:13:09 -0300

    Fix: atualizando script do banco de dados

--- src/database/vizor.sql
+++ src/database/vizor.sql
@@ -6,16 +6,9 @@ USE vizor;
 -- =========================
 -- TABELAS BASE
 -- =========================
-CREATE TABLE pais(
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    nome VARCHAR(200) NOT NULL
-);
-
 CREATE TABLE estado(
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    nome VARCHAR(100) NOT NULL,
-    fkPais INT,
-    FOREIGN KEY (fkPais) REFERENCES pais(id)
+    nome VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE cidade(
@@ -23,11 +16,6 @@ CREATE TABLE cidade(
     nome VARCHAR(200) NOT NULL,
     fkEstado INT,
     FOREIGN KEY (fkEstado) REFERENCES estado(id)
-);
-
-CREATE TABLE zona(
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    zona VARCHAR(10) NOT NULL
 );
 
 -- =========================
@@ -59,7 +47,6 @@ CREATE TABLE endereco (
     fkCidade INT,
     fkMinipc INT,
     FOREIGN KEY (fkCidade) REFERENCES cidade(id),
-    FOREIGN KEY (fkZona) REFERENCES zona(id),
     FOREIGN KEY (fkMinipc) REFERENCES miniComputador(id)
 );
 
@@ -130,9 +117,6 @@ INSERT INTO usuario (nome, email, senha, cpf, telefone, fkEmpresa, fkCargo) VALU
 ('Ana Silva', 'ana@example.com', 'senha456', '10987654321', '11988887777', 2, 2),
 ('Carlos Pereira', 'carlos@example.com', 'senha789', '11223344556', '11977776666', 3, 3);
 
-INSERT INTO usuario(nome,email,senha,cpf,telefone,fkEmpresa,fkCargo) VALUES
-('Guilherme Leon', 'guilherme@example.com', 'senha123', '12345678901', '11999998888', 1, 1);
-
 INSERT INTO miniComputador (fkEmpresa, fkEndereco) VALUES
 (1, NULL),
 (2, NULL),
@@ -165,9 +149,6 @@ WHERE us.id = 1;
 SELECT id AS IdEmpresa, nome AS NomeEmpresa, codigoAtivacao AS Codigo 
 FROM empresa;
 
-SELECT * FROM empresa;
-SELECT * FROM usuario;
-
 INSERT INTO usuario (nome, email, senha, cpf, telefone, fkEmpresa, fkCargo) 
 VALUES (
     'Joao', 
@@ -179,11 +160,30 @@ VALUES (
     1
 );
 
-SELECT * FROM usuario;
-
 SELECT us.nome as NomeUsuario, us.email as EmailUsuario, us.telefone as Telefone, us.cpf as CPF, us.senha as SenhaUsuario 
 FROM usuario as us;
 
 SELECT * FROM usuario;
+SELECT * FROM empresa;
 
 SELECT us.id, us.nome, us.email, empresa.codigoAtivacao, us.fkcargo FROM usuario AS us INNER JOIN empresa ON empresa.id = us.fkEmpresa WHERE email = email AND senha = senha;
+
+SELECT
+    e.id AS IdEmpresa,
+    e.nome AS NomeEmpresa,
+    e.cnpj AS CNPJ,
+    e.codigoAtivacao AS CodigoAtivacao,
+    en.rua,
+    en.numero,
+    en.bairro,
+    en.cep,
+    c.nome AS Cidade,
+    es.nome AS Estado
+FROM
+    empresa AS e
+INNER JOIN
+    endereco AS en ON e.fkEndereco = en.id
+INNER JOIN
+    cidade AS c ON en.fkCidade = c.id
+INNER JOIN
+    estado AS es ON c.fkEstado = es.id;
