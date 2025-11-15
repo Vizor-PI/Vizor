86a1556314cbaf180bc3070a47fa4c96bc0f2087
VitorioBearari <vitorio.bearari@sptech.school>
2025-11-15 14:50:01 -0300

    Adicionando metodo main e funcao  pegarEventosExtremos

--- public/vitorioDashboard/weatherbit.js
+++ public/vitorioDashboard/weatherbit.js
@@ -12,3 +12,56 @@ async function pegarClimaAtual() {
         icone: clima.weather.icon
     }
 }
+
+
+async function pegarEventosExtremos() {
+    const resp = await fetch("https://api.weatherbit.io/v2.0/alerts?key=8e71021053ed4cb0b006ce4560638438&city=Sao%20Paulo&country=BR&lang=pt");
+    const json = await resp.json();
+
+    const cidade = json.city_name;
+    const alertas = json.alerts; // ja vem array
+
+    // Se não tiver alertas
+    if (alertas.length === 0) {
+        return {
+            cidade: cidade,
+            tem_alertas: false,
+            mensagem: "Sem eventos climáticos extremos...",
+            alertas: []
+        };
+    }
+
+    // Se tiver alertas (improvavel)
+    const alertasTratados = [];
+
+    for (let i = 0; i < alertas.length; i++) {
+        const a = alertas[i];
+
+        alertasTratados.push({
+            titulo: a.title,
+            descricao: a.description,
+            severidade: a.severity,
+            inicio_local: a.effective_local,
+            fim_local: a.expires_local,
+            fonte: a.alerts, 
+            uri: a.uri
+        });
+    }
+
+    return {
+        cidade: cidade,
+        tem_alertas: true,
+        mensagem: null,
+        alertas: alertasTratados
+    };
+}
+
+async function main() {
+    const climaAtual = await pegarClimaAtual();
+    const eventosExtremos = await pegarEventosExtremos();
+
+    console.log("Clima Atual:", climaAtual);
+    console.log("Eventos Extremos:", eventosExtremos);
+}
+
+main();
