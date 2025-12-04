9aa2b3e3990866b140d50de070d7a487e7142d5c
Hanieh24 <hanieh.ashouri@sptech.school>
2025-12-04 11:18:30 -0300

    mudando o route para ele funciona local

--- siteMockado/src/routes/HaniehDash.js
+++ siteMockado/src/routes/HaniehDash.js
@@ -1,15 +1,45 @@
 var express = require("express");
 var router = express.Router();
+
 var HaniController = require("../controllers/HaniehDashController");
 
-router.post("/syncAlerts", (req, res) => HaniController.syncAlerts(req, res));
-router.get("/getKpis", (req, res) => HaniController.getKpis(req, res));
-router.get("/topModels", (req, res) => HaniController.topModels(req, res));
-router.get("/topLotes", (req, res) => HaniController.topLotes(req, res));
-router.get("/comparison", (req, res) => HaniController.comparison(req, res));
-router.get("/heatmap", (req, res) => HaniController.heatmap(req, res));
-router.get("/list", (req, res) => HaniController.list(req, res));
-router.get("/recommend", (req, res) => HaniController.recommend(req, res));
-router.get("/alerts/:type/:id", (req, res) => HaniController.listAlerts(req, res));
+router.post("/syncAlerts", function(req, res){
+    HaniController.syncAlerts(req,res)
+});
+
+
+router.get("/getKpis", function (req, res) {
+    HaniController.getKpis(req, res);
+});
+
+router.get("/topModels", function (req, res) {
+    HaniController.topModels(req, res);
+});
+
+router.get("/topLotes", function (req, res) {
+    HaniController.topLotes(req, res);
+});
+
+router.get("/comparison", function (req, res) {
+    HaniController.comparison(req, res);
+});
+
+router.get("/heatmap", function (req, res) {
+    HaniController.heatmap(req, res);
+});
+
+router.get("/list", function (req, res) {
+    HaniController.list(req, res);
+});
+
+router.get("/recommend", function (req, res) {
+    HaniController.recommend(req, res);
+});
+
+router.get("/alerts/:type/:id", function(req, res) {
+    HaniController.listAlerts(req, res)
+});
+
+
 
 module.exports = router;
